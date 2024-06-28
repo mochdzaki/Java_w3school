1a274789a19433f7c12608d4b61aba47f551b7e9
Niilua <m.dzaki.mif@gmail.com>
2024-06-28 14:51:53 +0700

    update JavaNumbers

---
+++ Intro Java/JavaNumbers.java
@@ -0,0 +1,28 @@
+// this file is to know there are type of variables that can provide a limit value in the value itself
+// this file is not to important, it just for someone that want to save their memory from the program
+
+public class JavaNumbers {
+    public static void main(String[] args){
+        // first type is for the int variable 
+        // first one is byte
+        byte myByte = 10; // this type of data just can provide from -128 until 127 numbers 
+        System.out.println(myByte);
+
+        // second is short
+        short myShort = -8000; // this type of data can provide whole numbers from -32768 to 32767
+        System.out.println(myShort);
+
+        // next one is int
+        // the usually data type that everyone use
+        int myInt = 2000000; // all may know the this data type can provide or store much numbers in it
+        System.out.println(myInt);
+
+        // the lasst one is long
+        // this data type is used for data that not large enough instead using int
+        long myLong = 12222999999L; // this data type could provide more thatn 1 biliion numbers in it
+        System.out.println(myLong);
+
+
+        // 
+    }
+}
